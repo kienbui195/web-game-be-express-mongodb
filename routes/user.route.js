1e417612cfc8558fe33cdedce723522e9c74983b
Kay Bi <kaybi9x@gmail.com>
2022-10-20 10:04:40 +0700

    add

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,8 +2,10 @@ const { controller } = require('../controllers/controller');
 const router = require('express').Router();
 
 //Add user register
-router.post('/register', controller.saveUserRegisterData(req, res).catch(err => console.log(err)));
-router.post('/login', controller.postLogin(req, res).catch(err => console.log(err)));
-router.post('/user/info', controller.getUserInfo(req, res).catch(err => console.log(err)));
+router.post('/register', controller.saveUserRegisterData);
+
+router.post('/login', controller.postLogin);
+
+router.post('/user/info', controller.getUserInfo);
 
 module.exports = router;
